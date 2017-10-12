227d2af1584db63c56f5543d01704b891068803b
Jakub Głuszecki <jakub.gluszecki@gmail.com>
2017-10-12 11:49:26 +0200

    Filter tokens

--- app/shows_repository.rb
+++ app/shows_repository.rb
@@ -1,14 +1,20 @@
 class ShowsRepository
   include Import['db']
 
-  QUERY = <<~SQL
-    select s.id, s.title, ts_rank(to_tsvector(s.title), to_tsquery(?)) as rank 
+  QUERY = <<~SQL.freeze
+    select s.id, s.title, ts_rank(to_tsvector(s.title), to_tsquery(?)) as rank
     from shows s order by rank desc limit 1
   SQL
 
+  TOKEN_BLACKLIST = %w[www torrenting com 720p 1080p webrip bluray x264 strife].freeze
+
   def find_by_title(title)
-    tokens = title.scan(/\w+/)
+    tokens = filter_tokens(title.scan(/\w+/))
     rs = db.fetch(QUERY, tokens.join(' & ')).first
     rs[:rank] > 0.01 ? rs : nil
+  end
+
+  def filter_tokens(tokens)
+    tokens.map(&:downcase).reject { |e| e =~ /s\d+e\d+/ } - TOKEN_BLACKLIST
   end
 end
